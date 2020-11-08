5b281ab52bb84d374a8e33f8702e64e2cdd6584c
samis <samilsully@gmail.com>
2020-11-07 21:12:38 -0800

    fixed link to different pages on routes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Yay Books</a>
+  <Link className="navbar-brand" href="./">Yay Books</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse justify-content-right" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a className="nav-link" href="#">Search Books</a>
+        <Link className="nav-link" to="./">Search Books</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Saved Books</a>
+        <Link className="nav-link" href="./saved">Saved Books</Link>
       </li>
     </ul>
   </div>
