457ccb8a80343c501f5bedce418311cb87f34268
samis <samilsully@gmail.com>
2020-11-07 20:47:13 -0800

    added back in base page files

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Card = () => {
+    return (
+        <div>
+            <h1>This is the search page</h1>
+            <div className="card">
+                <div className="sm-col-4">
+        <div className="card-body">
+          <h5 className="card-title">This is a Book</h5>
+          <p className="card-text">Book info goes here yay</p>
+          <a href="#" className="btn btn-primary">Save</a><a href="#" className="btn btn-primary">Delete</a>
+        </div>
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default Card;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,12 +9,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
